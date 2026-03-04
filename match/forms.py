edc4557e26131bb85689f0632cb64ab115e491b5
Vladimirhoa <rozhdestvo.vi@mail.ru>
2026-03-04 12:56:24 +0300

    Переработана регистрация

--- match/forms.py
+++ match/forms.py
@@ -18,3 +18,11 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = ""
+        if 'password1' in self.fields:
+            self.fields['password1'].help_text = ""
+        if 'password2' in self.fields:
+            self.fields['password2'].help_text = ""
