be39dd3bd139df20afa8f5825ba21e23f262e720
Vladimirhoa <rozhdestvo.vi@mail.ru>
2026-03-05 03:04:42 +0300

    Загрузка напрямую из вк

--- video/models.py
+++ video/models.py
@@ -2,21 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 from match.models import Match
 
+
 class Video(models.Model):
     STATUS_CHOICES = [
         ('processing', 'В очереди / Обрабатывается'),
         ('ready', 'Готово'),
         ('error', 'Ошибка'),
     ]
+
     title = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
     video = models.FileField(upload_to='videos/%Y/%m/%d', blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='videos', null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='processing')
 
     class Meta:
         verbose_name = "Видео"
         verbose_name_plural = "Видео"
+
     def __str__(self):
         return f"{self.title} (Матч:{self.match})"
